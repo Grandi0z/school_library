1d827adf091d952f851cd202bc928d35b7759d23
Grandi0z <josephlkhausi@gmail.com>
2023-09-14 11:25:46 +0200

    Correct files importation

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,7 @@
-require './person'
-require './student'
-require './teacher'
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
 
-Person.new(4, 'Rex', false)
+Person.new(4, 'Rex', parent_permission: false)
 Student.new(18, 'A1', 'Malick')
 Teacher.new(85, 'Math', 'Henry')
