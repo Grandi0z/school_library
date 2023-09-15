0f532aa15a7e7ca010c122849ed8bee006f852e1
Grandi0z <josephlkhausi@gmail.com>
2023-09-15 22:35:13 +0200

    for debug

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,7 @@ class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(0..1000)
-    @name = name
+    @name = "John"
     @age = age
     @parent_permission = parent_permission
     @rentals = []
